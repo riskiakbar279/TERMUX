6af4e77ddf06c8c54b3bcfb92e12a4d2d625b77f
riskiakbar279 <70522253+riskiakbar279@users.noreply.github.com>
2021-07-03 11:39:16 +0700

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,4 @@
+#install bahan Untuk Termux UMUM maupun khusus
+# code : riskiakbar279
+
+pkg install nano; pkg install mc; pkg install git; pkg install python ; pkg install python2 ; pkg install dirhunt; pkg install curl; pkg install wget; pkg install php; pkg install bs4
